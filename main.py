18241ccce39d37ed95dfeebe3a26cebb7cf71e2b
goloneczka <48791689+goloneczka@users.noreply.github.com>
2022-03-13 17:43:35 +0100

    doing commit

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ if __name__ == '__main__':
         print("Event: %s %s" % (event['type'], event['object'].metadata.name))
 
         if event['type'] == 'ADDED':
-            print("Object added: ", event['object'].metadata.managed_fields.fields_v1['f:spec'])
+            print(event)
+            print("Object added: ", event['object'].spec.nodeName)
 
         if event['type'] == 'DELETED':
-            print("Object deleted: ", event['object'].metadata.managed_fields.fields_v1['f:spec'])
+            print("Object deleted: ", event['object'].spec.nodeName)
 
         if datetime.datetime.now() > minute_after_timestamp:
             w.stop()
