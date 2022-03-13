14dc7544d32857f3af529a5613bda0a4355df27b
goloneczka <48791689+goloneczka@users.noreply.github.com>
2022-03-13 16:43:37 +0100

    add basic watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from kubernetes import client, config
+import datetime
+
+from kubernetes import client, config, watch
 
 if __name__ == '__main__':
 
@@ -8,3 +10,15 @@ if __name__ == '__main__':
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+    minute_after_timestamp = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    w = watch.Watch()
+    for event in w.stream(v1.list_namespace, _request_timeout=60):
+        print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+        if event['type'] == 'ADDED':
+            print('event type is added')
+        if datetime.datetime.now() > minute_after_timestamp:
+            w.stop()
+
+    print("Ended.")
+
