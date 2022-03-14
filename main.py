313b4084b868d29bedeccecc401eb3b83be05806
goloneczka <48791689+goloneczka@users.noreply.github.com>
2022-03-14 19:22:46 +0100

    development ...

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from kubernetes import client, config, watch
 from WatchListener import watch_pod_events
 
 if __name__ == '__main__':
-    config.load_incluster_config()
+    config.load_kube_config()
     watch_pod_events()
